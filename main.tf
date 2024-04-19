ab0913f2928bdf4be3f6007db31f7386d3387780
wvandergryp <162649334+wvandergryp@users.noreply.github.com>
2024-04-18 22:35:34 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,22 +17,6 @@
 # On vm1 - ssh 10.0.2.2 ping 10.0.2.2 - SSH into vm1 and ping its own IP address.
 # On vm2 - ssh 10.0.1.2 ping 10.0.1.2 - SSH into vm2 and ping its own IP address.
 
-variable "gcp_project" {
-   description = "The ID of the project in which resources will be managed."
-   type        = string
-   default = "gcpvpnstrong"
-}
-
-variable "subnet_cidr_vpc1_sub1" {
-  description = "CIDR range for the subnet"
-  default     = "10.0.1.0/24"
-}
-
-variable "subnet_cidr_vpc2_sub1" {
-  description = "CIDR range for the subnet"
-  default     = "10.0.2.0/24"
-}
-
 provider "google" {
   project = var.gcp_project
   region  = "us-central1"
